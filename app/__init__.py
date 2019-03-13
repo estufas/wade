7e6f472911a1763b146a71c749bb7af7cb46bd7f
Stuart hiller <stuarthiller@Stuarts-MacBook-Pro-2.local>
2019-03-12 23:36:37 -0700

    we got the scrapper working

--- app/__init__.py
+++ app/__init__.py
@@ -33,28 +33,12 @@ tasks = [
 ]
 
 def scrape_aa_meetings():
-    page = requests.get('https://lacoaa.org/meetings/?tsml-day=4&tsml-distance=5&tsml-mode=location&tsml-query=90042')
-
-    soup = BeautifulSoup(page.content, 'html.parser')
-    # meeting_table = soup.findAll('div',attrs={"class":"row"})
-    # meetings = meeting_table.find_all('a')
+    page = requests.get('https://lacoaa.org/meetings/?tsml-day=any')
+    soup = BeautifulSoup(page.text, 'html.parser')
     rows = soup.find_all('tr')
     for row in rows:          # Print all occurrences
-        print(row.get_text())
-    # print (meeting_table)
+        print('help', row.get_text())
 
-    # textContent = []
-    # for i in range(0, 10):
-    #     paragraphs = soup.find_all("a")[i].text
-    #     textContent.append(paragraphs)
-
-    # Take out the <div> of name and get its value
-    # name_box = soup.select("td.name")
-    # test = name_box.a
-
-
-    # print ('ok')
-    print (textContent)
 
 
 def create_app(config_name):
@@ -65,12 +49,13 @@ def create_app(config_name):
 
     @app.route('/meetings', methods=['GET'])
     def get_tasks():
-        print ('api')
-        scrape_aa_meetings()
+        # print ('api')
+
         return jsonify({'tasks': tasks})
 
     @app.route('/')
     def hello_world():
+        scrape_aa_meetings()
         return 'Hello, Worrld!'
 
     return app
