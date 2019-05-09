bc8e27f912a40842e9cd286f421085e6acb583d7
Stuart hiller <stuarthiller@Stuarts-MacBook-Pro-2.local>
2019-05-09 00:41:49 -0700

    parsing soup object

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, jsonify
 import pdb
+import attr
 # import libraries
 # import urllib2
 import requests
@@ -24,17 +25,27 @@ from config import app_config
 tasks = [
     {
         'id': 1,
-        'title': u'Buy groceries',
+        'title': u'Buy eeihepoihwetihgroceries',
         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
         'done': False
     },
     {
         'id': 2,
-        'title': u'Learn Python',
+        'title': u'Learffdn Python',
         'description': u'Need to find a good Python tutorial on the web',
         'done': False
     }
 ]
+@attr.s(slots=True, kw_only=True)
+class TestObject(object):
+    time = attr.ib()
+    name = attr.ib()
+    location = attr.ib()
+    region = attr.ib()
+    type = attr.ib()
+
+big_list = []
+
 
 def scrape_aa_meetings():
     page = requests.get('https://lacoaa.org/meetings/?tsml-day=any')
@@ -42,7 +53,17 @@ def scrape_aa_meetings():
     rows = soup.find_all('tr')
     # for row in rows:          # Print all occurrences
     #     print('help', row.get_text())
-    # return rows
+    for idx,tr in rows:
+        for td in tr:
+            if td.find_all('span'):
+                meeting_time = td.find_all('span')
+                for item in meeting_time:
+                    print(item.string)
+            elif td.find('a'):
+                print(idx,td.find('a').string)
+            else:
+                print(idx,td.string)
+    return rows
 
 
 
@@ -55,10 +76,21 @@ def create_app(config_name):
     print(mongo)
     # db.init_app(app)
 
-    @app.route('/meetings', methods=['GET'])
+    @app.route('/meetings')
     def get_tasks():
         # print ('api')
-        # meetings = scrape_aa_meetings()
+        meetings = scrape_aa_meetings()
+        for idx,tr in enumerate(meetings[0:2]):
+            for td in tr:
+                if td.find_all('span'):
+                    span_list = td.find_all('span')
+                    for item in span_list:
+                        print(item.string)
+                elif td.find('a'):
+                    print(idx,td.find('a').string)
+                else:
+                    print(idx,td.string)
+
         return jsonify({'tasks': tasks})
 
     @app.route('/')
