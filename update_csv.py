e1cbc0d6d622095890a9e6769ec2a78da4351f24
imovius <ian.movius@gmail.com>
2019-06-13 22:41:23 -0700

    cleaned up data and included script which did it

---
+++ update_csv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+basedir = os.path.dirname(os.path.abspath(__file__))
+print(basedir)
+df = pd.read_csv(os.path.join(basedir,'computerRank10.csv'))
+# print(df.head())
+# corrected_time_dseries = df['0']+' '+df['Time']
+# print(df.head())
+# df['Time']= corrected_time_dseries
+# print(corrected_time_dseries.head())
+df.drop(['Distance','District'],inplace=True,axis=1)
+df.rename(index=str, columns={"Meeting": "Meeting_Name","0": "Day"},inplace=True)
+df.to_csv(os.path.join(basedir,'cleaned_data.csv'))
+print(df.head())
